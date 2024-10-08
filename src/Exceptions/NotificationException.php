463f1704bda0e8976231c820ce5e0835192a7c74
Saeed Yari <saeedyari.work@gmail.com>
2024-10-08 14:38:30 +0330

    Implement basic structure of sms routing and sending pattern

---
+++ src/Exceptions/NotificationException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Syaritech\Notify\Exceptions;
+
+use Exception;
+use Throwable;
+
+/**
+ *
+ */
+class NotificationException extends Exception
+{
+    /**
+     * @param string         $message
+     * @param int            $code
+     * @param Throwable|null $previous
+     */
+    public function __construct(string $message, int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
--- src/Facades/Notify.php
+++ src/Facades/Notify.php
@@ -4,8 +4,14 @@ namespace Syaritech\Notify\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @mixin \Syaritech\Notify\Notify
+ */
 class Notify extends Facade
 {
+    /**
+     * @return string
+     */
     protected static function getFacadeAccessor(): string
     {
         return 'notify';
